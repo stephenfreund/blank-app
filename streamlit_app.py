13c70e323f08cfbf3882737a62cfe0e26f775475
Stephen Freund <stephenfreund@users.noreply.github.com>
2024-11-15 09:24:12 +0000

    Push

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
+from tempfile import TemporaryFile
 import streamlit as st
+
 
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+with TemporaryFile() as f:
+    f.write("Boop")
